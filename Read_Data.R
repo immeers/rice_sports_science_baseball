de91594680bddb4b2d85eb59fc2afa3a96e9ee24
alett19 <abl10@rice.edu>
2023-10-30 10:33:28 -0500

    Reads specific file

--- Read_Data.R
+++ Read_Data.R
@@ -1,2 +1,2 @@
-baseballdata <- read.table(file.choose(),header=T,sep="") # Default is sep=""
+baseballdata <- read.csv("2023_10_05_17_26_21_Rice_Pitching_Lab_44_Jackson_Mayo_Home.report (1).txt", sep="\t") # Default is sep=""
 View(baseballdata)
