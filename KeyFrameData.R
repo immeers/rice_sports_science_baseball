5db702d741c1267302bc65411d99ac5dad16a696
alett19 <abl10@rice.edu>
2023-11-07 16:51:03 -0600

    Function for getting keyframe data

--- KeyFrameData.R
+++ KeyFrameData.R
@@ -1,18 +1,21 @@
-keyframedata <- read.csv("motion_tracker_result_parameters.csv", sep=",") # Default is sep=""
-View(keyframedata)
-KeyFrameEvent <- c("Start", "MaximumLeadingLegLift", "HandsApart", "ArmsOut", "LeadingFootStrike", "MaximumExternalShoulderRotation", "Acceleration", "BallRelease", "MaximumInternalShoulderRotation", "Deceleration", "MaximumTrailingLegLift", "End")
-
-keyframedata$AreKeyFramesDetected <- gsub('[\\{\\}]', '', keyframedata$AreKeyFramesDetected)
-AreKeyFramesDetected <- c(strsplit(keyframedata$AreKeyFramesDetected, ";"))
-
-keyframedata$KeyFrameIndices <- gsub('[\\{\\}]', '', keyframedata$KeyFrameIndices)
-KeyFrameIndices <- c(strsplit(keyframedata$KeyFrameIndices, ";"))
-
-keyframedata$KeyFrameDetectionScores <- gsub('[\\{\\}]', '', keyframedata$KeyFrameDetectionScores)
-KeyFrameDetectionScores <- c(strsplit(keyframedata$KeyFrameDetectionScores, ";"))
-
-Tracked <- c(keyframedata$Tracked)
-
-KeyFrameDF <- data.frame(KeyFrameEvent, AreKeyFramesDetected, KeyFrameIndices, KeyFrameDetectionScores, Tracked)
-colnames(KeyFrameDF) = c("KeyFrameEvent", "AreKeyFramesDetected", "KeyFrameIndices", "KeyFrameDetectionScores", "Tracked")
-View(KeyFrameDF)
+get_keyframe_data <- function(filename){
+  keyframedata <- read.csv(filename, sep=",") # Default is sep=""
+  KeyFrameEvent <- c("Start", "MaximumLeadingLegLift", "HandsApart", "ArmsOut", "LeadingFootStrike", "MaximumExternalShoulderRotation", "Acceleration", "BallRelease", "MaximumInternalShoulderRotation", "Deceleration", "MaximumTrailingLegLift", "End")
+  
+  keyframedata$AreKeyFramesDetected <- gsub('[\\{\\}]', '', keyframedata$AreKeyFramesDetected)
+  AreKeyFramesDetected <- c(strsplit(keyframedata$AreKeyFramesDetected, ";"))
+  
+  keyframedata$KeyFrameIndices <- gsub('[\\{\\}]', '', keyframedata$KeyFrameIndices)
+  KeyFrameIndices <- c(strsplit(keyframedata$KeyFrameIndices, ";"))
+  
+  keyframedata$KeyFrameDetectionScores <- gsub('[\\{\\}]', '', keyframedata$KeyFrameDetectionScores)
+  KeyFrameDetectionScores <- c(strsplit(keyframedata$KeyFrameDetectionScores, ";"))
+  
+  Tracked <- c(keyframedata$Tracked)
+  
+  KeyFrameDF <- data.frame(KeyFrameEvent, AreKeyFramesDetected, KeyFrameIndices, KeyFrameDetectionScores, Tracked)
+  colnames(KeyFrameDF) = c("KeyFrameEvent", "AreKeyFramesDetected", "KeyFrameIndices", "KeyFrameDetectionScores", "Tracked")
+  View(KeyFrameDF)
+  return(KeyFrameDF)
+}
+keyframe_df = get_keyframe_data("motion_tracker_result_parameters.csv")
