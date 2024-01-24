8bb1a28f72845ddaa50c87f4f0a45e97c8ac4be3
immim <imm6@rice.edu>
2024-01-24 11:01:36 -0600

    fixed merge and changed function order, see hand angular velocity df i added date to col header

--- mlb_pitches.R
+++ mlb_pitches.R
@@ -1,11 +1,11 @@
 install.packages("dplyr")
 install.packages("tidyr")
+install.packages("stringr")
 library("dplyr")
 library("tidyr")
 library(ggplot2)
 library(magrittr)
-
-
+library(stringr)
 
 #' read_data
 #'
@@ -13,13 +13,11 @@ library(magrittr)
 #' @param filename path of file as string
 #' @return returns df of data in file
 #' @examples
-read_data <- function(filename){
+read_data <- function(filename, date){
   baseballdata <- read.csv(filename, sep="\t") # Default is sep=""
   #View(baseballdata)
   return(baseballdata)
 }
-#baseballdata = read_data('C:/Users/immim/OneDrive/Rice/Sports Science/rice_sports_science_baseball/to be analyzed/to be analyzed/2024_01_09_09_56_21_Rice_Pitching_Lab_0_Matt_Canterino_Home.report.txt')
-baseballdata = read_data("2023_10_05_17_26_21_Rice_Pitching_Lab_44_Jackson_Mayo_Home.report (1).txt")
 
 #' get_metric_data
 #'
@@ -40,15 +38,6 @@ get_metric_data <-function(baseballdata){
   #View(MetricData)
   return(MetricData)
 }
-
-#call above function
-metric_df = get_metric_data(baseballdata)
-
-#melted_df <- metric_df %>%
-# pivot_longer(everything(), names_to = "variable", values_to = "value")
-
-#View(melted_df)
-
 
 #' transform_metric_data
 #'
@@ -72,8 +61,6 @@ transform_metric_data <- function(metric_df){
   return(transposed_df)
 }
 
-metric_df = transform_metric_data(metric_df)
-
 get_event_data <-function(baseballdata){
   n <- 0
   EventData <- data.frame(matrix(ncol = ncol(baseballdata), nrow = 5))
@@ -87,7 +74,7 @@ get_event_data <-function(baseballdata){
   #View(EventData)
   return(EventData)
 }
-event_df = get_event_data(baseballdata)
+
 
 get_ts_data <-function(baseballdata){
   n <- 0
@@ -99,10 +86,8 @@ get_ts_data <-function(baseballdata){
     }
   }
   TSData <- Filter(function(x)!all(is.na(x)), TSData) # Removes empty columns
-  View(TSData)
   return(TSData)
 }
-ts_df = get_ts_data(baseballdata)
 
 get_derived_data <-function(baseballdata){
   n <- 1
@@ -118,7 +103,7 @@ get_derived_data <-function(baseballdata){
   #View(DerivedData)
   return(DerivedData)
 }
-derived_df = get_derived_data(baseballdata)
+
 
 get_kinematic_data <-function(baseballdata){
   
@@ -132,21 +117,19 @@ get_kinematic_data <-function(baseballdata){
   View(KinematicData)
   return(KinematicData)
 }
-kinematic_df = get_kinematic_data(baseballdata)
 
-get_hand_kinematic_sequence <- function(baseballdata){
+get_hand_kinematic_sequence <- function(baseballdata, date_str){
   
   index <- which(kinematic_df=="Pitching_Hand_KinematicSequence", arr.ind=TRUE)
   item <- kinematic_df[,1]
   handcolumn <- kinematic_df[,index[1,2]]
   hand_kinematic_data <- data.frame(item, as.numeric(handcolumn))
-  colnames(hand_kinematic_data) <- c("Frame Number", "Hand Angular Velocity")
+  colnames(hand_kinematic_data) <- c("Frame Number", paste("Hand Angular Velocity", date_str))
   hand_kinematic_data <- tail(hand_kinematic_data, -4)
   View(hand_kinematic_data)
   return(hand_kinematic_data)
   
 }
-hand_kinematic_sequence_df = get_hand_kinematic_sequence(baseballdata)
 
 get_pelvis_kinematic_sequence <- function(baseballdata){
   
@@ -160,7 +143,6 @@ get_pelvis_kinematic_sequence <- function(baseballdata){
   return(pelvis_kinematic_data)
   
 }
-pelvis_kinematic_sequence_df = get_pelvis_kinematic_sequence(baseballdata)
 
 get_trunk_kinematic_sequence <- function(baseballdata){
   
@@ -174,7 +156,6 @@ get_trunk_kinematic_sequence <- function(baseballdata){
   return(trunk_kinematic_data)
   
 }
-trunk_kinematic_sequence_df = get_trunk_kinematic_sequence
 
 get_upperarm_kinematic_sequence <- function(baseballdata){
   
@@ -188,7 +169,6 @@ get_upperarm_kinematic_sequence <- function(baseballdata){
   return(upperarm_kinematic_data)
   
 }
-upperarm_kinematic_sequence_df = get_upperarm_kinematic_sequence(baseballdata)
 
 get_all_kinematic_sequences <- function(baseballdata) {
   s = 0
@@ -210,7 +190,6 @@ get_all_kinematic_sequences <- function(baseballdata) {
   View(kinematic_sequences_data_frame)
   return(kinematic_sequences_data_frame)
 }
-all_kinematic_sequences_df = get_all_kinematic_sequences(baseballdata)
 
 get_max_hand_vel_frame <- function(baseballdata){
   max_hand_vel <- max(hand_kinematic_sequence_df$`Hand Angular Velocity`, na.rm = TRUE)
@@ -218,8 +197,6 @@ get_max_hand_vel_frame <- function(baseballdata){
   max_hand_vel_frame <- hand_kinematic_sequence_df[i[1],1]
   return(max_hand_vel_frame)
 }
-
-max_hand_velocity_frame = get_max_hand_vel_frame(baseballdata)
 
 get_keyframe_data <- function(filename){
   keyframedata <- read.csv(filename, sep=",") # Default is sep=""
@@ -243,25 +220,100 @@ get_keyframe_data <- function(filename){
   return(KeyFrameDF)
   
 }
-keyframe_df = get_keyframe_data("motion_tracker_result_parameters.csv")
 
-if (keyframe_df$KeyFrameIndices[8] != max_hand_velocity_frame){
-  print("Ball Release Not Concurrent With Max Hand Angular Velocity")
-}
+# Specify the directory path
+directory_path <- "C:/Users/immim/OneDrive/Rice/Sports Science/to be analyzed/to be analyzed/"
 
-ggplot(data = hand_kinematic_sequence_df, aes(x = `Frame Number`, y = `Hand Angular Velocity`)) + geom_point() + geom_vline(xintercept=keyframe_df$KeyFrameIndices, linetype="solid", colour = "red")
+# Get a list of file names in the directory
+files <- list.files(path = directory_path)
 
-longkinematicdf = all_kinematic_sequences_df %>% pivot_longer(cols=c('Pelvis_KinematicSequence', 'Trunk_KinematicSequence', 'Pitching_UpperArm_KinematicSequence', 'Pitching_Hand_KinematicSequence'), names_to='KinematicSequence',values_to='AngularVelocity')
-ggplot(data = longkinematicdf, aes(x = FrameNumber, y = AngularVelocity)) + geom_point(aes(color=KinematicSequence)) + geom_vline(xintercept=keyframe_df$KeyFrameIndices, linetype="solid", colour = "red")
-
-#PitchInfo <- read.csv(("Mayo_Fastball_85.6_15.1_-9.3_TM_Metadata.txt"), sep="\t")
-#PitchInfo <- read.csv(("C:/Users/immim/OneDrive/Rice/Sports Science/pitching data/Mayo_Fastball_85.6_15.1_-9.3_TM_Metadata.txt"), sep="\t")
-
-#View(PitchInfo)
-View(all_kinematic_sequences_df)
-View(longkinematicdf)
-
-add_to_master_df <- function(filename){
-  master_hand_df <- data.frame()
+# Loop over each file
+for (file in files) {
+  # Construct the full path to the file
+  full_path <- file.path(directory_path, file)
   
-}
+  
+  
+  # Define the regex pattern
+  pattern <- "(\\d{4}_\\d{2}_\\d{2}_\\d{2}_\\d{2}_\\d{2})"
+  
+  # Use str_extract to extract the matching pattern
+  date <- str_extract(file, pattern)
+  
+  # Print the result
+  
+  file_extension <- tools::file_ext(file)
+  
+  if (file_extension %in% c("txt", "TXT")) {
+    # CSV file
+    #read_data(file, date)
+    print(date)
+    print(file)  
+
+
+    
+    baseballdata = read_data(full_path, date)
+    #baseballdata = read_data("2023_10_05_17_26_21_Rice_Pitching_Lab_44_Jackson_Mayo_Home.report (1).txt")
+    
+    metric_df = get_metric_data(baseballdata)
+    
+    metric_df = transform_metric_data(metric_df)
+    
+
+    event_df = get_event_data(baseballdata)
+    
+
+    ts_df = get_ts_data(baseballdata)
+    
+
+    derived_df = get_derived_data(baseballdata)
+    
+
+    kinematic_df = get_kinematic_data(baseballdata)
+    
+
+    hand_kinematic_sequence_df = get_hand_kinematic_sequence(baseballdata, date)
+    
+
+    pelvis_kinematic_sequence_df = get_pelvis_kinematic_sequence(baseballdata)
+    
+
+    trunk_kinematic_sequence_df = get_trunk_kinematic_sequence
+    
+
+    upperarm_kinematic_sequence_df = get_upperarm_kinematic_sequence(baseballdata)
+    
+
+    all_kinematic_sequences_df = get_all_kinematic_sequences(baseballdata)
+    
+
+    
+    max_hand_velocity_frame = get_max_hand_vel_frame(baseballdata)
+    
+
+    keyframe_df = get_keyframe_data('C:/Users/immim/OneDrive/Rice/Sports Science/to be analyzed/to be analyzed/2023_12_22_09_48_29_Rice_Pitching_Lab_0_Matt_Canterino_motion_tracker_frame_result_parameters.csv')
+    if (keyframe_df$KeyFrameIndices[8] != max_hand_velocity_frame){
+      print("Ball Release Not Concurrent With Max Hand Angular Velocity")
+    }
+    ggplot(data = hand_kinematic_sequence_df, aes(x = `Frame Number`, y = `Hand Angular Velocity`)) + geom_point() + geom_vline(xintercept=keyframe_df$KeyFrameIndices, linetype="solid", colour = "red")
+    
+    #pivots cols in all kinematic sequences
+    longkinematicdf = all_kinematic_sequences_df %>% pivot_longer(cols=c('Pelvis_KinematicSequence', 'Trunk_KinematicSequence', 'Pitching_UpperArm_KinematicSequence', 'Pitching_Hand_KinematicSequence'), names_to='KinematicSequence',values_to='AngularVelocity')
+    View(longkinematicdf)
+    ggplot(data = longkinematicdf, aes(x = FrameNumber, y = AngularVelocity)) + geom_point(aes(color=KinematicSequence)) + geom_vline(xintercept=keyframe_df$KeyFrameIndices, linetype="solid", colour = "red")
+    
+    #PitchInfo <- read.csv(("Mayo_Fastball_85.6_15.1_-9.3_TM_Metadata.txt"), sep="\t")
+    #PitchInfo <- read.csv(("C:/Users/immim/OneDrive/Rice/Sports Science/pitching data/Mayo_Fastball_85.6_15.1_-9.3_TM_Metadata.txt"), sep="\t")
+    
+    #View(PitchInfo)
+    View(all_kinematic_sequences_df)
+    View(longkinematicdf)
+    
+    add_to_master_df <- function(filename){
+      master_hand_df <- data.frame()
+  
+    }
+  }
+
+  }
+  
